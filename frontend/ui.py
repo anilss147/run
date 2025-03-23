b00f60315cf3d0dd9f6e9d3bfbc7e305cee9aec5
A-Bosh <anilsherikar207@gmail.com>
2025-03-23 17:04:24 +0530

    Update ui.py

--- frontend/ui.py
+++ frontend/ui.py
@@ -1,4 +1,3 @@
-# frontend/ui.py
 import gradio as gr
 import logging
 import os
@@ -9,7 +8,7 @@ logging.basicConfig(filename=log_file_path, level=logging.INFO, format='%(asctim
 
 logging.info("Starting Gradio application...")
 
-print("Gradio launch initiated") # added print statement
+print("Gradio launch initiated")
 
 gr.Interface(lambda x: x, "text", "text").launch(share=False, server_name="127.0.0.1", server_port=7861, debug=True)
 
