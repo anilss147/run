6d6cc2d287e1445fdf4859dc01b8a4d4d0eccb19
A-Bosh <anilsherikar207@gmail.com>
2025-03-23 15:55:45 +0530

    Update ui.py

--- frontend/ui.py
+++ frontend/ui.py
@@ -1,16 +1,7 @@
 import gradio as gr
-import logging
-import os
-
-log_file_path = os.path.join(os.getcwd(), 'gradio.log') # Get current working directory.
-
-logging.basicConfig(filename=log_file_path, level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-
-logging.info("Starting Gradio application...")
 
 def greet(name):
-    logging.info(f"Received input: {name}")
     return "Hello, " + name + "!"
 
 iface = gr.Interface(fn=greet, inputs="text", outputs="text")
-iface.launch()
+iface.launch(share=False, server_name="127.0.0.1")
